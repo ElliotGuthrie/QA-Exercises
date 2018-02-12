b27b472b1542cb26bbdda973d44edd2edcbeaddd
ElliotGuthrie <elliot@guthrie.biz>
2018-02-12 17:05:14 +0000

    Paint Wizard done

--- src/Level_2_Intermediate_PaintWizard/PaintCalculations.java
+++ src/Level_2_Intermediate_PaintWizard/PaintCalculations.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class PaintCalculations
 {
 	private ArrayList<PaintProduct> paints;
+	int roomSize = 40;
 	
 	public PaintCalculations()
 	{
@@ -13,7 +14,6 @@ public class PaintCalculations
 	public static void main(String[] args)
 	{
 		PaintCalculations paintCalcs = new PaintCalculations();
-		int roomSize = 40;
 		
 		PaintProduct paint1 = new PaintProduct("CheapoMax", 20, 19.99, 10);
 		PaintProduct paint2 = new PaintProduct("AverageJoes", 15, 17.99, 11);
@@ -22,17 +22,20 @@ public class PaintCalculations
 		paintCalcs.addPaint(paint1);
 		paintCalcs.addPaint(paint2);
 		paintCalcs.addPaint(paint3);
+		
+		paintCalcs.wastageCalc();
 	}
 	public void addPaint(PaintProduct paints)
 	{
 		this.paints.add(paints);
 	}
-	public void wastageCalc(int roomSize, int paintVolume, int paintCoverage)
+	public void wastageCalc()
 	{
 		for(int i = 0; i < paints.size(); i++ )
 		{
-			double wastage = paintVolume - (roomSize / paintCoverage);
-			System.out.println("For a room of size " + roomSize + "m^2, " + paintName + " will waste " + wastage + "L of paint.");
+			PaintProduct p = paints.get(i);
+			double wastage = p.paintVolume - (roomSize / p.paintCoverage);
+			System.out.println("For a room of size " + roomSize + "m^2, " + p.paintName + " will waste " + wastage + "L of paint.");
 		}
 	}
 }
